634585c2390e5a4a84963d156ab75c2c87a13e9e
hoosierhuy <hoosierhuy@outlook.com>
2019-02-05 14:37:43 -0500

    Converted some components into feature modules

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
+
+@NgModule({
+  declarations: [
+    SigninComponent,
+    SignupComponent
+  ],
+  imports: [
+    SharedModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule {
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { DropdownDirective } from './dropdown.directive';
+
+@NgModule({
+  declarations: [
+    DropdownDirective
+  ],
+  exports: [
+    CommonModule,
+    DropdownDirective,
+    FormsModule
+  ]
+})
+export class SharedModule {}
