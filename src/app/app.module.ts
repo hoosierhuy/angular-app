b333ddaa1e89d41f5a19e9a0f8ed4aa04093b762
hoosierhuy <hoosierhuy@outlook.com>
2019-02-06 11:45:27 -0500

    Removed unused imports, provided option for persistance layer requiring bearer token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
-import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing-module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -23,7 +16,6 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
     HttpClientModule,
     ShoppingListModule,
     AppRoutingModule,
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 import { secrets } from '../../secrets';
@@ -13,7 +13,7 @@ export class DataStorageService {
   apiUrl = secrets.apiEndpoint;
 
   constructor(
-    private http: HttpClient,
+    private httpClient: HttpClient,
     private recipeService: RecipeService,
     private authService: AuthService
   ) {}
@@ -21,16 +21,19 @@ export class DataStorageService {
   storeRecipes$() {
     const token = this.authService.getToken();
     // Put method works well with my persistence service
-    return this.http.put(`${this.apiUrl}?auth=${token}`, this.recipeService.getRecipes());
+    return this.httpClient.put(`${this.apiUrl}?auth=${token}`, this.recipeService.getRecipes() /* if bearer token is required, {
+      observe: 'body',
+      headers: new HttpHeaders().set('Authorization', 'Bearer token123abcyadayada')
+    }*/);
   }
 
   getRecipes$() {
     const token = this.authService.getToken();
 
-    this.http.get(`${this.apiUrl}?auth=${token}`)
+    this.httpClient.get<RecipeModel[]>(`${this.apiUrl}?auth=${token}`)
       .pipe(
         map(
-          (recipes: RecipeModel[]) => {
+          (recipes) => {
             return recipes.map(recipe => {
               if (!recipe['ingredients']) {
                 recipe.ingredients = [];
