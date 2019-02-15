27e219ec4c1c61e7051fc31eaaad6ab2b98af222
hoosierhuy <hoosierhuy@outlook.com>
2019-02-14 21:49:40 -0500

    Started migration to BootStrap 4.3

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, HostListener, HostBinding, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.open')
-  isOpen = false;
 
-  @HostListener('click')
-  toggleOpen() {
+constructor (private el: ElementRef, private renderer: Renderer2) {}
+
+@HostBinding('class.show') isOpen = false;
+
+@HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
-  }
+    const part = this.el.nativeElement.querySelector('.dropdown-menu');
+    if (this.isOpen) {
+      this.renderer.addClass(part, 'show');
+    } else {
+      this.renderer.removeClass(part, 'show');
+    }
+}
 }
